360b58f089800dc3282bd2548d48da07e055fcae
mschen_ <441864148@qq.com>
2026-09-23 11:42:04 +0800

    修复 OpenAPI v2 命名空间兼容性

--- src/Dorisoy.Meeting.Web/Startup.cs
+++ src/Dorisoy.Meeting.Web/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using Dorisoy.Meeting.Server;
 using Dorisoy.Meeting.Server.Authorization;
 
